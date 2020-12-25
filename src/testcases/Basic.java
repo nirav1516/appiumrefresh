dc74b11717d0ca15d2d6cda4f35fba574956a6b9
Nirav Shah <nirav1516@gmail.com>
2020-12-25 15:50:14 -0800

    Diffrent examples of dinding element

--- src/testcases/Basic.java
+++ src/testcases/Basic.java
@@ -1,7 +1,10 @@
 package testcases;
 
 import java.net.MalformedURLException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 
 import init.Setup;
 import io.appium.java_client.android.AndroidDriver;
@@ -13,6 +16,27 @@ public class Basic extends Setup{
 		// TODO Auto-generated method stub
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		AndroidElement e=  driver.findElementByXPath("//android.widget.TextView[@text='Preference']");
+		e.click();
+		
+		e=  driver.findElementByXPath("//android.widget.TextView[@content-desc='3. Preference dependencies']");
+		e.click();
+		
+		e = driver.findElementById("android:id/checkbox");		
+		e.click();
+		
+		e=  driver.findElementByXPath("(//android.widget.RelativeLayout)[2]");
+		e.click();
+		
+		e = driver.findElement(By.className("android.widget.EditText"));
+		e.setValue("Nirav");
+		 
+		//e= driver.findElement(By.id("android:id/button1"));
+		
+		List<AndroidElement> eL = driver.findElementsByClassName("android.widget.Button");
+		e = eL.get(1);
+		e.click();
 	}
 
 }
