b62e5ae77f49113ffd23f595dd3b6b3234513277
Nirav Shah <nirav1516@gmail.com>
2020-12-28 01:50:09 -0800

    Removed unused import

--- src/testcases/Gesturebasic1.java
+++ src/testcases/Gesturebasic1.java
@@ -1,7 +1,6 @@
 package testcases;
 
 import java.net.MalformedURLException;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
@@ -11,6 +10,7 @@ import init.Setup;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import static java.time.Duration.ofSeconds;
+
 import io.appium.java_client.android.AndroidElement;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 
